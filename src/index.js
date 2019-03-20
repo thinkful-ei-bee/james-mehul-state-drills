d43920f6e1dc31b875c4c87b20a49a58a2f04ae4
likejackonfire <gravitybringsmedown@gmail.com>
2019-03-20 11:37:19 -0700

    accordion.js initial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './state-drills/Accordion';
+import sections from './sectionIndex';
 import './index.css';
 
 
-ReactDOM.render(<Accordion />, document.getElementById('root'));
+ReactDOM.render(<Accordion sections = {sections} />, document.getElementById('root'));
--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 class Accordion extends React.Component {
     static defaultProps = {
-        bulletInChamber: 8, 
+       
       }
-    constructor(props) {
-        super(props)
-        this.state = {
-            chamber:null,
-            spinningTheChamber:false
-           
-        }
-      }
+   
+    state = {
+      currentSection: null,
+    }
 
       handleTriggerClick = () => {
         this.setState({
@@ -30,42 +26,27 @@ class Accordion extends React.Component {
         clearInterval(this.timeout)
       }
 
+      renderButtons () {
+         return (
+          this.props.sections.map((section, index) => (
+          <li><button key={index}>
+            {section.name}
+          </button>
+          </li>
+        )) 
+      }
+
     render() {
-         this.state.bulletInChamber = this.props.bulletInChamber
 
-        if(this.state.chamber === null)
-        {
-            return (<div>
-                <button onClick={() => this.handleTriggerClick()}>
-             Pull the trigger!
-           </button>
-          </div>
-          )
-        }
-        else if(this.state.spinningTheChamber === true){
-           return (
-           <div><p>spinning the chamber and pulling the trigger! .... </p></div>
-           )
-        }
-        else if(this.state.chamber === this.state.bulletInChamber)
-        {
-            return (
-                <div><p>Bang</p></div>
 
-            )
-        }
-        else{
             return (
-                <div><p>You're safe! Bullet in {this.state.bulletInChamber} and you pulled {this.state.chamber}</p>
-                 <button onClick={() => this.handleTriggerClick()}>
-                Pull the trigger!
-              </button>
-                </div>
+               <ul>
+                 {buttons}
+               </ul>
                
 
             )
-        }
-    
+        
   }
 }
 
